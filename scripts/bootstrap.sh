5ecd9c6ff4ad29d46a33fbea33424b2434df2898
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2019-10-13 19:10:37 +1300

    Added installation for go and kind

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,45 +1,60 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+install_curl()
+{
+	# install docker dependencies
+	sudo apt-get update -y && sudo apt-get install -y --allow-unauthenticated --no-install-recommends \
+	curl &&
+	apt-get clean
+}
 
 install_kubectl()
 {
-    # TODO Pin kubectl version
-    # Download and move kubectl into /usr/local/bin
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-    chmod +x ./kubectl
-    sudo mv ./kubectl /usr/local/bin/
+	# TODO Pin kubectl version
+	# Download and move kubectl into /usr/local/bin
+	curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+	chmod +x ./kubectl
+	sudo mv ./kubectl /usr/local/bin/
 }
 
 install_docker()
 {
-    # install docker dependencies
-    sudo apt-get update -y && sudo apt-get install -y --allow-unauthenticated --no-install-recommends \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common &&
-    apt-get clean
+	# install docker dependencies
+	sudo apt-get update -y && sudo apt-get install -y --allow-unauthenticated --no-install-recommends \
+	apt-transport-https \
+	ca-certificates \
+	software-properties-common &&
+	apt-get clean
 
-    # Install docker
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	# Install docker
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-    sudo add-apt-repository \
-        "deb https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) \
-        stable"
+	# TODO: 13/10/2019 - Morteza, we either use Linux Mint or Arch Linux as our development environment. For those using Linux Mint, following command
+	# won't work. Instead of running "lsb_release -cs", we need to retrieve UBUNTU_CODENAME from /etc/os-release
+	sudo add-apt-repository \
+	"deb https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
 
-    # TODO Pin docker version
-    sudo apt-get install -y --allow-unauthenticated docker-ce
+	# TODO Pin docker version
+	sudo apt-get install -y --allow-unauthenticated docker-ce docker-ce-cli containerd.io
 }
 
 install_go()
 {
-    # TODO Implement if needed.
-    echo "Not implemented."
+	curl -Lo go.tar.gz https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
+	sudo tar -C /usr/local -xzf go.tar.gz
+	rm go.tar.gz
+
+	echo "Make sure you add /usr/local/go to your $PATH and set your $GOPATH environment variable correctly"
+
 }
 
 install_kind()
 {
-
+	curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-linux-amd64
+	chmod +x ./kind
+	sudo mv ./kind /usr/local/bin/kind
 }
 
 ###
@@ -49,17 +64,18 @@ install_kind()
 # Ensure that this bootstrap is running on an Ubuntu machine
 cat /etc/os-release | grep -i ubuntu
 if [[ $? -ne 0 ]]; then
-    echo "This bootstrap was made for Ubuntu machines only. Please manually install the dependencies."
+	echo "This bootstrap was made for Ubuntu machines only. Please manually install the dependencies."
 fi
 
 # List of dependencies that are required to deploy a K8s cluster locally with KIND
-readonly dependencies="kubectl docker kind"
+readonly dependencies="curl kubectl docker go kind"
+
 for dep in $dependencies; do
-    command -v $dep &>/dev/null
-    if [[ $? -ne 0 ]]; then
-        echo "$dep does not exist. Installing $dep"
-        install_$dep
-    fi
+	command -v $dep &>/dev/null
+	if [[ $? -ne 0 ]]; then
+		echo "$dep does not exist. Installing $dep"
+		install_$dep
+	fi
 done
 
 

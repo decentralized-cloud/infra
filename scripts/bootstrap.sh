7525afd10f42d097e7b6ceaed6733756c9d5cdcb
Morteza Alizadeh <morteza.alizadeh@iag.co.nz>
2021-01-24 21:32:01 +1300

    Removing /etc/apt/sources.list.d/kubernetes.list file prior to installing kubectl

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -15,6 +15,7 @@ install_curl()
 
 install_kubectl()
 {
+    sudo rm -f /etc/apt/sources.list.d/kubernetes.list
     sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
     echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list

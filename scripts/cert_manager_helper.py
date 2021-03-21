a4b8bd556870103b558aa2f8117608a179e4b0d2
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2021-03-21 22:33:43 +1300

    Read godaddy API key and secret from env file

--- scripts/cert_manager_helper.py
+++ scripts/cert_manager_helper.py
@@ -1,6 +1,9 @@
 from os import path
+from os import remove
+from os import environ
 from config_helper import ConfigHelper
 from system_helper import SystemHelper
+import tempfile
 
 
 class CertManagerHelper:
@@ -37,6 +40,20 @@ class CertManagerHelper:
             self.system_helper.execute(
                 "helm upgrade --install godaddy-webhook --namespace cert-manager {godaddy} --wait".format(godaddy=godaddy))
 
-            self.system_helper.execute(
-                "kubectl apply -n istio-system -f \"{config_file}\"".format(config_file=path.join(
-                    self.config_helper.get_config_root_directory(), "common", "cert-manager", "godaddy-clusterissuers.yaml")))
+            apikey_secret = environ["GODADDY_API_SECRET"]
+            tmp_file_name = path.join(tempfile._get_default_tempdir(), next(
+                tempfile._get_candidate_names()))
+
+            with open(
+                path.join(
+                    self.config_helper.get_config_root_directory(), "common", "cert-manager", "godaddy-clusterissuers.yaml")) as file:
+                content = file.read()
+                content = content.replace('<key:secret>', apikey_secret)
+                tmp_config_file = open(tmp_file_name, "w")
+                tmp_config_file.write(content)
+                tmp_config_file.close()
+
+                self.system_helper.execute(
+                    "kubectl apply -n istio-system -f \"{config_file}\"".format(config_file=tmp_file_name))
+
+                remove(tmp_config_file)

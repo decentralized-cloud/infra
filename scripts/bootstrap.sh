4346fc7a168c579b694c000b4907a3f33af69741
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2021-12-06 19:25:16 +1300

    Updated golang version

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -70,7 +70,7 @@ install_docker()
 
 install_go()
 {
-    curl -Lo go.tar.gz https://dl.google.com/go/go1.17.3.linux-amd64.tar.gz
+    curl -Lo go.tar.gz https://dl.google.com/go/go1.17.4.linux-amd64.tar.gz
     sudo rm -rf /usr/local/go
     sudo tar -C /usr/local -xzf go.tar.gz
     rm go.tar.gz
